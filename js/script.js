a1adc70b085fe7b073eb477d78d90149d0a76f21
bealabea <beatricemarin89@yahoo.it>
2022-01-17 15:38:45 +0100

    adds items in the "todo list", clicking in the text adds or removes the line-through class, clicking the X removes the text from the view

---
+++ js/script.js
@@ -0,0 +1,48 @@
+new Vue({
+    el: "#app",
+    data: {
+        todo: [
+            {
+                text:'Fare la spesa',
+                done: true
+            },
+            {
+                text:'Fare il bucato',
+                done: true
+            },
+            {
+                text:'Studiare',
+                done: false
+            },
+            {
+                text:'Andare in palestra',
+                done: false
+            },
+            {
+                text:'Chiamare il dottore',
+                done: false
+            },
+        ],
+        // variabile per salvare il testo digitato nell'input html
+        temporaryItem: '',
+    },
+    methods:{
+        // funzione per aggiungere all'array un nuovo oggetto
+        addItem: function(){
+            this.todo.push({text:this.temporaryItem, done:false})
+            this.temporaryItem = '';
+        },
+        // funzione per rimuovere un oggetto dall'array
+        removeItem: function(index){
+            this.todo.splice(index, 1)
+        },
+        // funzione per cambiare la proprietà done degli oggetti nell'array
+        changeDone: function(index){
+            if(this.todo[index].done){
+                this.todo[index].done = false;
+            } else {
+                this.todo[index].done = true;
+            }
+        }
+    }
+});
