ddcbfc09b68cab9f4e4d70927085ef74a847a483
bealabea <beatricemarin89@yahoo.it>
2022-01-17 16:01:56 +0100

    adds style and controls the text in the input

--- js/script.js
+++ js/script.js
@@ -1,48 +1,53 @@
 new Vue({
-    el: "#app",
-    data: {
-        todo: [
-            {
-                text:'Fare la spesa',
-                done: true
-            },
-            {
-                text:'Fare il bucato',
-                done: true
-            },
-            {
-                text:'Studiare',
-                done: false
-            },
-            {
-                text:'Andare in palestra',
-                done: false
-            },
-            {
-                text:'Chiamare il dottore',
-                done: false
-            },
-        ],
-        // variabile per salvare il testo digitato nell'input html
-        temporaryItem: '',
+  el: "#app",
+  data: {
+    todo: [
+      {
+        text: "Fare la spesa",
+        done: true,
+      },
+      {
+        text: "Fare il bucato",
+        done: true,
+      },
+      {
+        text: "Studiare",
+        done: false,
+      },
+      {
+        text: "Andare in palestra",
+        done: false,
+      },
+      {
+        text: "Chiamare il dottore",
+        done: false,
+      },
+    ],
+    // variabile per salvare il testo digitato nell'input html
+    temporaryItem: "",
+  },
+  methods: {
+    // funzione per aggiungere all'array un nuovo oggetto
+    addItem: function () {
+        // controllo input
+      if (this.temporaryItem === '') {
+        alert("inserisci il testo");
+      } else {
+        this.todo.push({ text: this.temporaryItem, done: false });
+      }
+      this.temporaryItem = "";
     },
-    methods:{
-        // funzione per aggiungere all'array un nuovo oggetto
-        addItem: function(){
-            this.todo.push({text:this.temporaryItem, done:false})
-            this.temporaryItem = '';
-        },
-        // funzione per rimuovere un oggetto dall'array
-        removeItem: function(index){
-            this.todo.splice(index, 1)
-        },
-        // funzione per cambiare la proprietà done degli oggetti nell'array
-        changeDone: function(index){
-            if(this.todo[index].done){
-                this.todo[index].done = false;
-            } else {
-                this.todo[index].done = true;
-            }
-        }
-    }
+    // funzione per rimuovere un oggetto dall'array
+    removeItem: function (index) {
+      this.todo.splice(index, 1);
+    },
+    // funzione per cambiare la proprietà done degli oggetti nell'array
+    changeDone: function (index) {
+      if (this.todo[index].done) {
+        this.todo[index].done = false;
+      } else {
+        this.todo[index].done = true;
+      }
+    },
+  },
 });
